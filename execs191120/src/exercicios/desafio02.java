fafc3cc053991e1b459f2dd2fa65cc3b81e732ec
Erick-S <ericksblumberg@gmail.com>
2019-12-03 21:31:17 -0300

    Desafio02, adições extras

--- execs191120/src/exercicios/desafio02.java
+++ execs191120/src/exercicios/desafio02.java
@@ -5,6 +5,9 @@
  */
 package exercicios;
 
+// Importet pattern enum so every time it is used here it will not be required
+// to write 'pattern."PATTERN".value'
+//import static exercicios.desafio02.Pattern.*;
 import java.util.Random;
 
 /**
@@ -58,70 +61,248 @@ public class desafio02 {
         [ X ' X ]
         [ X ' X ]
     
+     * Different method to print faces: arrays for rows:
+     * there are 5 different rows that the dice use:
+     * can make the patterns of 0 to 6 in square dice faces.
+    
+     * [ ' ' ' ] || Empty
+     * [ ' X ' ] || Middle
+     * [ X ' X ] || Double
+     * [ X ' ' ] || Left
+     * [ ' ' X ] || Right
+    
+     * OR
+     
+     * there are 6 different columns that the dice use:
+     * can make the patters of 0 to 9 in square dice faces.
+    
+     * [ ' ' ' ] || Empty
+     * [ ' X ' ] || Middle
+     * [ X ' X ] || Double
+     * [ X X X ] || Fill
+     * [ X ' ' ] || Up
+     * [ ' ' X ] || Down
+    
+     * Usage of rows[]
+    
+     * die[X] = rowArray;
+    
+     * switch(value)
+     * case X:
+     *      die[0] = patternY;
+     *      die[1] = patternY;
+     *      die[2] = patternY;
+    
+     * Usage of columns[]
+    
+     * //fill the column?
+     * int column = 0;
+     * for(int row = 0; var < die[column].length; row++){
+     *     die[column][row] = pattern[row];
+     * }
+    
+     * OR
+    
+     * create another dimension of array with all the faces EX:
+     * char die[6][3][3] // for a six sided die
+     * die[0]: // die 1
+     * [ . . . ]
+     * [ . X . ]
+     * [ . . . ]
+     * ...
+    
      */
+    
+    // The different row patterns
+//    static char[][] patterns = 
+//        {
+//            {'.', '.', '.'}, // pattern[0] == Empty line
+//            {'.', 'X', '.'}, //pattern[1] == Middle eye
+//            {'X', '.', 'X'}, //pattern[2] == Double eyes
+//            {'X', '.', '.'}, //pattern[3] == Left eye
+//            {'.', '.', 'X'} //pattern[4] == Right eye
+//        };
+    
+    // enum to name the value of the patterns
+//    enum Pattern {
+//        EMPTY(0), MIDDLE(1), DOUBLE(2), LEFT(3), RIGHT(4);
+//        
+//        private final int value;
+//        
+//        Pattern(int value){
+//            this.value = value;
+//        }
+//    }
     
     public static void main(String args[]){
         
         // values from {0...5}+1, so {1...6}
         int randomValue = new Random().nextInt(6)+1;
+        
+        // die face
         char[][] die = new char[3][3];
-        
-        // Creates an empty die
-        for(int row = 0; row < die.length; row++){
-            for(int column = 0; column < die[row].length; column++){
-                die[row][column] = ' ';
+
+        char[][][] dies = {
+            {   // Die face value 1
+                {'.','.','.'},
+                {'.','X','.'},
+                {'.','.','.'}
+            },
+            {   // Die face value 2
+                {'X','.','.'},
+                {'.','.','.'},
+                {'.','.','X'}
+            },
+            {   // Die face value 3
+                {'X','.','.'},
+                {'.','X','.'},
+                {'.','.','X'}
+            },
+            {   // Die face value 4
+                {'X','.','X'},
+                {'.','.','.'},
+                {'X','.','X'}
+            },
+            {   // Die face value 5
+                {'X','.','X'},
+                {'.','X','.'},
+                {'X','.','X'}
+            },
+            {   // Die face value 6
+                {'X','.','X'},
+                {'X','.','X'},
+                {'X','.','X'}
             }
-        }
+        };
         
+        // with array of patterns, this is not needed
+        // Creates an empty die
+//        for(int row = 0; row < die.length; row++){
+//            for(int column = 0; column < die[row].length; column++){
+//                die[row][column] = '.'; // Fill empty spaces with ' character
+//            }
+//        }
+
         //DEBUG
-        randomValue = 6;
-        
+        //randomValue = 6;
+
         switch(randomValue){
-            default: 
-                System.out.print("An error ocurred! Invalid value!\n");
+            default:
+                System.out.println("An error ocurred! Invalid value!");
+                System.exit(0);
                 break;
             case 1:
-                die[1][1] = 'X';
+                die = dies[0];
                 break;
             case 2:
-                die[0][0] = 'X';
-                die[2][2] = 'X';
+                die = dies[1];
                 break;
             case 3:
-                die[0][0] = 'X';
-                die[1][1] = 'X';
-                die[2][2] = 'X';
+                die = dies[2];
                 break;
             case 4:
-                die[0][0] = 'X';
-                die[2][0] = 'X';
-                die[0][2] = 'X';
-                die[2][2] = 'X';
+                die = dies[3];
                 break;
             case 5:
-                die[0][0] = 'X';
-                die[2][0] = 'X';
-                die[1][1] = 'X';
-                die[0][2] = 'X';
-                die[2][2] = 'X';
+                die = dies[4];
                 break;
             case 6:
-                die[0][0] = 'X';
-                die[1][0] = 'X';
-                die[2][0] = 'X';
-                die[0][2] = 'X';
-                die[1][2] = 'X';
-                die[2][2] = 'X';
+                die = dies[5];
                 break;
         }
+        
+        // Instead of creating the pattern in the switch case, it will be
+        // returned from the patterns array, utilizing the pattern enum as
+        // a guide
+//        switch(randomValue){
+//            default:
+//                System.out.println("An error ocurred! Invalid value!");
+//                System.exit(0);
+//                break;
+//            case 1:
+//                // pattern. ommited because of the import of the enum Pattern
+//                die[0] = patterns[/*Pattern.*/EMPTY.value];
+//                die[1] = patterns[MIDDLE.value];
+//                die[2] = patterns[EMPTY.value];
+//                break;
+//            case 2:
+//                die[0] = patterns[LEFT.value];
+//                die[1] = patterns[EMPTY.value];
+//                die[2] = patterns[RIGHT.value];
+//                break;
+//            case 3:
+//                die[0] = patterns[LEFT.value];
+//                die[1] = patterns[MIDDLE.value];
+//                die[2] = patterns[RIGHT.value];
+//                break;
+//            case 4:
+//                die[0] = patterns[DOUBLE.value];
+//                die[1] = patterns[EMPTY.value];
+//                die[2] = patterns[DOUBLE.value];
+//                break;
+//            case 5:
+//                die[0] = patterns[DOUBLE.value];
+//                die[1] = patterns[MIDDLE.value];
+//                die[2] = patterns[DOUBLE.value];
+//                break;
+//            case 6:
+//                die[0] = patterns[DOUBLE.value];
+//                die[1] = patterns[DOUBLE.value];
+//                die[2] = patterns[DOUBLE.value];
+//                break;
+//        }
+        
+     
+        // switch case with hardcoded patterns
+        // Requires the array to be initialized, otherwise it will be empty
+        // (Unless you want the other spaces to be empty characters, wich can
+        // cause some of the patterns to break).
+//        switch(randomValue){
+//            default: 
+//                System.out.println("An error ocurred! Invalid value!");
+//                System.exit(0);
+//            case 1:
+//                die[1][1] = 'X';
+//                break;
+//            case 2:
+//                die[0][0] = 'X';
+//                die[2][2] = 'X';
+//                break;
+//            case 3:
+//                die[0][0] = 'X';
+//                die[1][1] = 'X';
+//                die[2][2] = 'X';
+//                break;
+//            case 4:
+//                die[0][0] = 'X';
+//                die[2][0] = 'X';
+//                die[0][2] = 'X';
+//                die[2][2] = 'X';
+//                break;
+//            case 5:
+//                die[0][0] = 'X';
+//                die[2][0] = 'X';
+//                die[1][1] = 'X';
+//                die[0][2] = 'X';
+//                die[2][2] = 'X';
+//                break;
+//            case 6:
+//                die[0][0] = 'X';
+//                die[1][0] = 'X';
+//                die[2][0] = 'X';
+//                die[0][2] = 'X';
+//                die[1][2] = 'X';
+//                die[2][2] = 'X';
+//                break;
+//        }
         
         System.out.printf("The random value is: %d\nThe die:\n", randomValue);
         
         // Prints the array
-        for(char[] row : die){
+        for(char[] column : die){
             System.out.print("[");
-            for(int column = 0;column < row.length; column++){
-                System.out.print(" "+row[column]);
+            for(int row = 0;row < column.length; row++){
+                System.out.print(" "+column[row]);
             }
             System.out.println(" ]");
         }
